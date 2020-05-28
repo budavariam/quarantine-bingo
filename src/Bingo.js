2c0dd7db23a3f95eb639988c6cc05cbbd42c5447
Mátyás Budavári <budavariam@gmail.com>
2020-05-28 22:11:33 +0200

    Store cross data in the matrix

--- src/Bingo.js
+++ src/Bingo.js
@@ -5,19 +5,10 @@ import { Cell } from "./Cell"
 
 export const Bingo = ({ phrases }) => {
     const lineLength = Math.sqrt(phrases.length)
-    const lines = phrases.reduce((acc, curr, i) => {
-        const currentLine = Math.trunc(i / lineLength)
-        if (i % lineLength === 0) {
-            return acc.length ? [...acc, [curr]] : [[curr]]
-        }
-        acc[currentLine].push(curr)
-        return acc
-    }, [])
 
-    const [crossedState, crossDispatch] = useReducer((state, action) => {
-        const newCrossMatrix = {...state.crossMatrix}
-        newCrossMatrix[action] = true
-        console.log(state, action, newCrossMatrix)
+    const [state, crossDispatch] = useReducer((state, { r, c }) => {
+        const newCrossMatrix = [...state.crossMatrix]
+        newCrossMatrix[r][c].checked = true
         return {
             ...state,
             crossMatrix: newCrossMatrix,
@@ -25,22 +16,33 @@ export const Bingo = ({ phrases }) => {
     }, {
         crossMatrix: {},
         bingoCount: 0,
+    }, () => {
+        return {
+            crossMatrix: phrases.reduce((acc, text, i) => {
+                const currentLine = Math.trunc(i / lineLength)
+                const cell = { text, checked: false }
+                if (i % lineLength === 0) {
+                    return acc.length ? [...acc, [cell]] : [[cell]]
+                }
+                acc[currentLine].push(cell)
+                return acc
+            }, [])
+        }
     })
 
     return (
         <table className="bingo">
             <tbody>
-                {lines.map((line, rowNum) => (
+                {state.crossMatrix.map((line, rowNum) => (
                     <tr key={rowNum}>
-                        {line.map((text, colNum) => {
-                            const cellIndex = `${rowNum}-${colNum}`
-                            console.log(cellIndex, crossedState.crossMatrix, crossedState.crossMatrix[cellIndex])
+                        {line.map(({ text, checked }, colNum) => {
+                            const cellIndex = { r: rowNum, c: colNum }
                             return (
                                 <td key={`${colNum}-${text}`}>
                                     <Cell
                                         text={text}
                                         index={cellIndex}
-                                        crossed={crossedState.crossMatrix[cellIndex]}
+                                        crossed={checked}
                                         crossDispatch={crossDispatch}
                                     />
                                 </td>)
